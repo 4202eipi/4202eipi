25abe17232412bd9d1f79d7eb0e3a44d6b3a68bf
hongsetoufaren <laurence.mclean@uqconnect.edu.au>
2015-08-17 10:53:32 +1000

    Sorting functions

---
+++ sort_positions.m
@@ -0,0 +1,17 @@
+function [ positions_out ] = sort_positions( positions_in, num_positions )
+    n = num_positions;
+    swapped = true;
+    while swapped==true
+        swapped = false;
+        for i=2:n
+            if(get_degree(positions_in(i,:)) < get_pan_degree(positions_in(i-1,:)))
+                temp = positions_in(i,:);
+                positions_in(i,:) = positions_in(i-1,:);
+                positions_in(i-1,:) = temp;
+                swapped = true;
+            end
+        end
+        n = n-1;
+    end
+    positions_out = positions_in;
+end
